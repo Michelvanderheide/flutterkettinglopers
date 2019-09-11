f782aba49076ff9a52c41a07092d131ba39a8977
Michel van der Heide <michelvanderheide@gmail.com>
2019-09-11 12:53:24 +0200

    development drill list

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+//import 'package:provider/provider.dart';
 
 import 'screens/sessions.dart';
 import 'screens/drills.dart';
@@ -14,6 +14,17 @@ void main() {
     );
 }
 
+class MyAppBar extends PreferredSize {
+  MyAppBar({Key key, Widget title}) : super(
+    key: key,
+    preferredSize: Size.fromHeight(30),
+    child: AppBar(
+      title: title,
+      // maybe other AppBar properties
+    ),
+  );
+}
+
 class KettinglopersApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -24,15 +35,31 @@ class KettinglopersApp extends StatelessWidget {
         child: Scaffold(
           body: TabBarView(
             children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  MyAppBar(title: Text("Instellingen")
+                  
+                  ),
+                  Expanded(
+                    child:Container(
+                      color: Colors.lightGreen,
+                    )
+                  ),
+                ],
+              ),
               DrillTabContainer(),
               SessionTabContainer(),
-              Container(
-                color: Colors.lightGreen,
-              )
+
             ],
           ),
           bottomNavigationBar: TabBar(
             tabs: [
+              Tab(
+                icon: Icon(Icons.settings),
+                text: "Instellingen"
+              ),
               Tab(
                 icon: Icon(Icons.directions_run),
                 text: "Oefeningen"
@@ -41,16 +68,13 @@ class KettinglopersApp extends StatelessWidget {
                 icon: Icon(Icons.event_note),
                 text: "Trainingen"
               ),
-              Tab(
-                icon: Icon(Icons.settings),
-                text: "Instellingen"
-              )
+
             ],
-            //labelColor: Colors.yellow,
-            unselectedLabelColor: Colors.blue[100],
+            labelColor: Colors.blue[700],
+            unselectedLabelColor: Colors.blue[300],
             indicatorSize: TabBarIndicatorSize.tab
           ),
-          backgroundColor: Colors.black,
+          backgroundColor: Colors.white,
         ),
       ),
     );
--- lib/screens/drills.dart
+++ lib/screens/drills.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutterdemo/models/drill.dart';
-import 'package:flutter_image/network.dart';
+//import 'package:flutter_image/network.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 class DrillTabContainer extends StatelessWidget {
   const DrillTabContainer({Key key}) : super(key: key);
@@ -11,44 +13,53 @@ class DrillTabContainer extends StatelessWidget {
       child: FutureBuilder<List<Drill>>(
         future: Drill.fetchAll(),
         builder: (context, snapshot) {
-
           if (snapshot.connectionState == ConnectionState.done) {
             return CustomScrollView(
               slivers: <Widget>[
                 const SliverAppBar(
+                  titleSpacing: 5.0,
                   pinned: false,
+                  floating: true,
+                  backgroundColor: Colors.white10,
+                  snap: false,
                   flexibleSpace: FlexibleSpaceBar(
-                    title: Text('Oefeningen'),
+                    title: Text('Oefeningen', style: TextStyle(fontSize: 13.0)),
                   ),
                 ),
                 SliverGrid(
                   gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
                     maxCrossAxisExtent: 200.0,
-                    mainAxisSpacing: 3.0,
-                    crossAxisSpacing: 3.0,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
                     childAspectRatio: 1.2,
                     
                   ),
                   delegate: SliverChildBuilderDelegate(
                     (BuildContext context, int index) {
-                      return Container(
-                        alignment: Alignment.bottomLeft,
-                        padding: const EdgeInsets.all(5.0),
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImageWithRetry(snapshot.data[index].imgUrl), 
-                            // image: CachedNetworkImage(
-                            //   imageUrl: snapshot.data[index].imgUrl,
-                            //   placeholder: (context, url) => new CircularProgressIndicator(),
-                            //   errorWidget: (context, url, error) => new Icon(Icons.error),
-                            // ),
-                            fit: BoxFit.cover),
-                        ),
-                        child: Text(snapshot.data[index].title, 
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(fontSize: 13,  fontWeight: FontWeight.normal)),
+
+                      final imagePovider = CachedNetworkImageProvider((snapshot.data[index].imgUrl=="" || snapshot.data[index].imgUrl==null ? "https://pbs.twimg.com/profile_images/945853318273761280/0U40alJG_400x400.jpg" : snapshot.data[index].imgUrl)
+                      , errorListener: () => {}
                       );
-                    }
+                        //print (snapshot.data[index].imgUrl);
+                        return  Container(
+                          alignment: Alignment.bottomLeft,
+                          padding: const EdgeInsets.all(3.0),
+                          decoration: BoxDecoration(
+                            image: DecorationImage(image: imagePovider, 
+                            fit: BoxFit.cover,
+                            colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.4), BlendMode.darken)),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Text(snapshot.data[index].title, 
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(fontSize: 13,  fontWeight: FontWeight.normal, color:Colors.white)),
+                            ],
+                          ) 
+                        );
+                    },
+                    childCount: snapshot.data.length // 216
                   ),
                 )
               ],
